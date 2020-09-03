38f83490a23f49d8c816c2ee16627a0f35d47782
kimoror <kimoror@mail.ru>
2020-09-03 21:55:30 +0300

    Add files

---
+++ src/ru/mirea/lab/Ball.java
@@ -0,0 +1,4 @@
+package ru.mirea.lab;
+
+public class Ball {
+}
---
+++ src/ru/mirea/lab/Main.java
@@ -0,0 +1,9 @@
+package ru.mirea.lab;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+	// write your code here
+    }
+}
