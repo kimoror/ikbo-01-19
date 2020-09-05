ddb64ef001ad9761c93da3e48cc8eafb10f47607
kimoror <kimoror@mail.ru>
2020-09-05 14:27:46 +0300

    Delete catalog lab_1-Желудков

--- lab_1-Желудков/src/ru/mirea/lab/Book.java
+++
@@ -1,8 +0,0 @@
-package ru.mirea.lab;
-
-public class Book {
-    private int pagesSize = 406;
-    public int getSize() {
-    return pagesSize;
-    }
-}
--- lab_1-Желудков/src/ru/mirea/lab/Dog.java
+++
@@ -1,9 +0,0 @@
-package ru.mirea.lab;
-
-public class Dog {
-    private String Name = "Bob";
-    public String  getName(){
-        return Name;
-    }
-
-}
