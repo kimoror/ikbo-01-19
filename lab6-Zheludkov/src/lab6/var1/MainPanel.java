392d3e008b3fd275ee462409e9c2682b40c3d4bb
kimoror <kimoror@mail.ru>
2020-10-06 10:50:42 +0300

    Add text area, button and some dialog message

--- lab6-Zheludkov/src/lab6/var1/MainPanel.java
+++ lab6-Zheludkov/src/lab6/var1/MainPanel.java
@@ -1,11 +1,20 @@
 package lab6.var1;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeListener;
 
 public class MainPanel extends JFrame{
     private JPanel[] mainPanel = new JPanel[9];;
     private JLabel head;
+    private JTextField numEnter;
+    private JButton ok;
+    private String rightNumber = "34";
+    private int inputNumber = 0;
+
     public MainPanel(){
         super("Quiz");
         setSize(720, 480);
@@ -18,8 +27,29 @@ public class MainPanel extends JFrame{
         }
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         head = new JLabel("Input your number:", SwingConstants.CENTER);
+        head.setFont(new Font("Dialog", Font.PLAIN, 16));
         head.setForeground(Color.BLACK);
         mainPanel[4].add(head, BorderLayout.CENTER);
+
+        numEnter = new JTextField();
+        mainPanel[4].add(numEnter, BorderLayout.SOUTH);
+        numEnter.setFont(new Font("Dialog", Font.PLAIN, 18));
+
+        ok = new JButton ("Ok");
+        ok.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(numEnter.getText().equals(rightNumber)){
+                    JOptionPane.showMessageDialog(null, "You are right!");
+                }
+                else{
+                    JOptionPane.showMessageDialog(null, "You are not right!");
+                }
+            }
+        });
+        mainPanel[7].add(ok, BorderLayout.NORTH);
+
+
         setVisible(true);
     }
 }
