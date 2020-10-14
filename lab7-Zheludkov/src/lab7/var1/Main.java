6f4ff481396aea606ba773eb21ca8f13bcea5788
kimoror <kimoror@mail.ru>
2020-10-14 11:26:31 +0300

    add remove and contain or doesnt contain num

--- lab7-Zheludkov/src/lab7/var1/Main.java
+++ lab7-Zheludkov/src/lab7/var1/Main.java
@@ -1,2 +1,32 @@
+package lab7.var1;
+
+import java.util.ArrayList;
+
 public class Main {
+
+    public static void print(ArrayList array){
+        for(Object it : array){
+            System.out.print(it +" ");
+        }
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<Integer>(7);
+        for(int i = 0; i < 7; i++){
+            arr.add((int)(Math.random() * 100));
+        }
+
+        print(arr);
+
+        arr.remove(6);
+        System.out.println("\nArray after remove:");
+        print(arr);
+
+        int rndNum = (int)(Math.random() * 100);
+        if(arr.contains(rndNum))
+            System.out.print("\nArray contain " + rndNum);
+        else
+            System.out.print("\nArray doesn`t contain " + rndNum);
+
+    }
 }
