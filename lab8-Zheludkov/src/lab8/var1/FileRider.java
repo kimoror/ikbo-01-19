839d5e079db86ae8f5e6562256098270b961ad78
kimoror <kimoror@mail.ru>
2020-10-14 13:10:00 +0300

    lab 8 var 1 is ready

---
+++ lab8-Zheludkov/src/lab8/var1/FileRider.java
@@ -0,0 +1,20 @@
+package lab8.var1;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class FileRider {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        try{
+            FileWriter writer = new FileWriter("/home/danila/MEGAsync/Institute/3-rd_semestr/ProgrammingInJava/Programming/ikbo-01-19/lab8-Zheludkov/TextFile.txt", false);
+            System.out.println("Please, input text:");
+            writer.write(sc.nextLine());
+            writer.flush();
+        }
+        catch (IOException e) {
+            System.out.println("File doesn`t exist");
+        }
+    }
+}
