46b151ba6ea094100fd28ad630da0b5a98f7a7fb
kimoror <kimoror@mail.ru>
2020-10-27 22:41:25 +0300

    add UnfairWaitList class

--- practice8-Zheludkov/src/pracitce8/UnfairWaitList.java
+++ practice8-Zheludkov/src/pracitce8/UnfairWaitList.java
@@ -1,5 +1,15 @@
 package pracitce8;
 
-public class UnfairWaitList<E> {
+public class UnfairWaitList<E> extends WaitList<E>{
 
+    public UnfairWaitList(){};
+
+    @Override
+    public E remove() {
+        return super.remove();
+    }
+
+    public void moveToBack(E element){
+        super.add(element);
+    }
 }
