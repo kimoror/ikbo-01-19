ab364a0479d07a6337b37b1f162494d4e8707f0d
kimoror <kimoror@mail.ru>
2020-10-28 09:51:44 +0300

    Task3 is ready

--- practice9-Zheludkov/src/practice9/Main.java
+++ practice9-Zheludkov/src/practice9/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         //Exception1.exeptionDemo();
         //Exception2.exceptionDemo();
-        Task3.exceptionDemo();
+        //Task3.exceptionDemo();
+        Task4.exceptionDemo();
     }
 }
