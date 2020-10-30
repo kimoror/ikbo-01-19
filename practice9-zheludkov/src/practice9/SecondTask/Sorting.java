91710366898881a478d75c8a14c4d44f89685c67
kimoror <kimoror@mail.ru>
2020-10-30 13:53:02 +0300

    Add Task2 ftom practice 9.Practice 9 is ready

---
+++ practice9-zheludkov/src/practice9/SecondTask/Sorting.java
@@ -0,0 +1,17 @@
+package practice9.SecondTask;
+
+public class Sorting {
+
+    public static void insertionSort(Comparable[] list){
+        int j;
+        for(int i = 0;i < list.length; i++){
+            j = i;
+            Comparable key = list[i];
+            while(j > 0 && key.compareTo(list[j-1]) > 0){
+                list[j] = list[j-1];
+                j--;
+            }
+            list[j] = key;
+        }
+    }
+}
